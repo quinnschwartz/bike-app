af159a42fe965e595420e0db3f07a2b8411553dd
Quinn Schwartz <quinnschwartz@gmail.com>
2017-01-05 09:31:55 -0800

    add for each loop to .text method in order to extract all bike models from the array of objects returned by the API.

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
   $('#bikeLocation').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=25&stolenness=proximity', function(response) {
-      $('.showBikes').text(response.bikes);
+    $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=25&stolenness=proximity').then(function(response) {
+      $('.showBikes').text(response.bikes.forEach(function(bike) {
+        $('.showBikes').append("<li>" + bike.title + "</li>");
+        console.log(response);
+      }));
     });
   });
 });
