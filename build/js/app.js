1969f440eff70a493b6de2aaeb08b02a9e1df515
Eric Bomblatus and Quinn Schwartz <eric@>
2017-01-05 14:06:40 -0800

    add date stolen

--- build/js/app.js
+++ build/js/app.js
@@ -4,9 +4,10 @@ Bike = function(city) {
 };
 
 Bike.prototype.getBikes = function(city) {
+  var baseUri = ('http://www.convert-unix-time.com/api?');
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + city + '&distance=25&stolenness=proximity').then(function(response) {
     $('.showBikes').text(response.bikes.forEach(function(bike) {
-      $('.showBikes').append("<li>" + bike.title + " " + bike.serial + "</li>");
+      $('.showBikes').append("<li>" + bike.title + " " + bike.serial + " " + new Date(bike.date_stolen * 1000) + "</li>");
       console.log(response);
     }));
   })
@@ -14,6 +15,12 @@ Bike.prototype.getBikes = function(city) {
       $('.$showBikes').text(error.responseJSON.message);
   });
 };
+
+function convertUnixTimeCallback(result){
+    console.log(result);
+}
+
+
 
 
 
